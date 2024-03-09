cf083f81c8227c4ae11a8b65809ec312197345cd
Andrew Burgess <andrew.burgess@classdojo.com>
2024-03-09 19:52:28 +0000

    chore(router): build setup tweaks

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,1 +1,3 @@
-export * from "./router";
+import createRouter from "./router";
+
+export default createRouter;
--- packages/router/vite.config.ts
+++ packages/router/vite.config.ts
@@ -3,6 +3,13 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, "src/index.ts"), formats: ["es"] } },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      name: "index",
+      fileName: "index",
+    },
+  },
   plugins: [dts()],
 });
