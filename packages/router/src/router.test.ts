69c50e64206b99d78c9d2f6bb2fea9d5a5e9f7ed
Andrew Burgess <andrew.burgess@classdojo.com>
2024-03-09 21:01:43 +0000

    fix(router): actually use the handler properly!

--- packages/router/src/router.test.ts
+++ packages/router/src/router.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from "vitest";
 import { type AddressInfo } from "node:net";
-import http from "node:http2";
+import http from "http";
 import createRouter from "./router";
 import { deferred } from "common";
 
-function getAddress(server: http.Http2Server) {
+function getAddress(server: http.Server) {
   const { promise, resolve, reject } = deferred<AddressInfo>();
 
   function get(orElse: () => void) {
@@ -21,24 +21,11 @@ function getAddress(server: http.Http2Server) {
 }
 
 test("router", async () => {
-  const server = http.createServer();
-  const router = createRouter(server);
+  const server = http.createServer().listen(0);
 
-  router.get("/", () => {
-    return "hello world";
-  });
+  createRouter(server).get("/", () => "hello world");
 
-  server.listen(0);
   const addr = await getAddress(server);
-  const client = http.connect(`http://localhost:${addr.port}`);
-  client.on("error", (err) => console.error(err));
-  let body = "";
-  const req = client.request({ ":path": "/" });
-  req.setEncoding("utf8");
-  req.on("data", (chunk) => console.log((body += chunk.toString())));
-  req.on("end", () => {
-    client.close();
-    expect(body).toEqual("hello world");
-  });
-  req.end();
+  const body = await fetch(`http://localhost:${addr.port}/`).then((r) => r.text());
+  expect(body).toEqual("hello world");
 });
--- packages/router/src/router.ts
+++ packages/router/src/router.ts
@@ -1,20 +1,24 @@
-import { type Http2Server } from "node:http2";
+import { type Server } from "http";
 
-export default function createRouter(server: Http2Server) {
+export default function createRouter(server: Server) {
   return Router.fromServer(server);
 }
 
 class Router {
-  #server: Http2Server;
-  static fromServer(server: Http2Server) {
+  #server: Server;
+  static fromServer(server: Server) {
     return new Router(server);
   }
 
-  private constructor(server: Http2Server) {
+  private constructor(server: Server) {
     this.#server = server;
   }
 
   get(_path: string, handler: () => void) {
-    this.#server.on("request", handler);
+    this.#server.on("request", (_req, res) => {
+      const body = handler();
+      res.write(body);
+      res.end();
+    });
   }
 }
