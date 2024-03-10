cb690d6b213e9045a6a66e0be04287b8ca907e3d
Andrew Burgess <andrew.burgess@classdojo.com>
2024-03-10 03:34:57 +0000

    fix(router): create test util

--- packages/router/src/router.test.ts
+++ packages/router/src/router.test.ts
@@ -3,6 +3,76 @@ import http from "http";
 import createRouter from "./router";
 import { deferred } from "@shaky/common";
 
+describe("router", () => {
+  it("responds", async () => {
+    const [client, server] = await getClientAndServer();
+
+    createRouter(server).get("/", () => "hello world");
+
+    const body = await client.get("/").then((r) => r.text());
+    expect(body).toEqual("hello world");
+  });
+
+  it("http methods can only match a given pattern once", async () => {
+    const [, server] = await getClientAndServer();
+
+    const router = createRouter(server);
+
+    router.get("/hello", () => "hello");
+
+    expect(() => {
+      router.get("/hello", () => "goodbye");
+    }).toThrowError("cannot register another handler for /hello");
+  });
+
+  it("matches routes", async () => {
+    const [client, server] = await getClientAndServer();
+
+    createRouter(server)
+      .get("/hello", () => "hello")
+      .get("/goodbye", () => "goodbye");
+
+    const body1 = await client.get("/hello").then((r) => r.text());
+    expect(body1).toEqual("hello");
+    const body2 = await client.get("/goodbye").then((r) => r.text());
+    expect(body2).toEqual("goodbye");
+  });
+
+  it("404s if no route matches", async () => {
+    const [client, server] = await getClientAndServer();
+
+    createRouter(server).get("/hello", () => "hello");
+
+    const statusCode = await client.get("/goodbye").then((r) => r.status);
+    expect(statusCode).toEqual(404);
+  });
+
+  it("let handlers set a status code", async () => {
+    const [client, server] = await getClientAndServer();
+
+    createRouter(server).get("/hello", () => [204, undefined]);
+
+    const req = await client.get("/hello");
+    expect(await req.text()).toEqual("");
+    expect(req.status).toEqual(204);
+  });
+});
+
+async function getClientAndServer() {
+  const server = http.createServer().listen(0);
+  const port = await getPort(server);
+  const baseUrl = `http://localhost:${port}`;
+
+  return [
+    {
+      get(path: string) {
+        return fetch(`${baseUrl}${path}`);
+      },
+    },
+    server,
+  ] as const;
+}
+
 function getPort(server: http.Server) {
   const { promise, resolve, reject } = deferred<number>();
 
@@ -16,60 +86,3 @@ function getPort(server: http.Server) {
 
   return promise;
 }
-
-describe("router", () => {
-  it("responds", async () => {
-    const server = http.createServer().listen(0);
-    createRouter(server).get("/", () => "hello world");
-
-    const port = await getPort(server);
-    const body = await fetch(`http://localhost:${port}/`).then((r) => r.text());
-    expect(body).toEqual("hello world");
-  });
-
-  it("http methods can only match a given pattern once", async () => {
-    const server = http.createServer().listen(0);
-    const router = createRouter(server);
-
-    router.get("/hello", () => "hello");
-
-    expect(() => {
-      router.get("/hello", () => "goodbye");
-    }).toThrowError("cannot register another handler for /hello");
-  });
-
-  it("matches routes", async () => {
-    const server = http.createServer().listen(0);
-
-    createRouter(server)
-      .get("/hello", () => "hello")
-      .get("/goodbye", () => "goodbye");
-
-    const port = await getPort(server);
-    const body1 = await fetch(`http://localhost:${port}/hello`).then((r) => r.text());
-    expect(body1).toEqual("hello");
-    const body2 = await fetch(`http://localhost:${port}/goodbye`).then((r) => r.text());
-    expect(body2).toEqual("goodbye");
-  });
-
-  it("404s if no route matches", async () => {
-    const server = http.createServer().listen(0);
-
-    createRouter(server).get("/hello", () => "hello");
-
-    const port = await getPort(server);
-    const statusCode = await fetch(`http://localhost:${port}/goodbye`).then((r) => r.status);
-    expect(statusCode).toEqual(404);
-  });
-
-  it("let handlers set a status code", async () => {
-    const server = http.createServer().listen(0);
-
-    createRouter(server).get("/hello", () => [204, undefined]);
-
-    const port = await getPort(server);
-    const req = await fetch(`http://localhost:${port}/hello`);
-    expect(await req.text()).toEqual("");
-    expect(req.status).toEqual(204);
-  });
-});
