758cff30b8e15f5bb361f9834ca2fc348d4c444c
Andrew Burgess <andrew.burgess@classdojo.com>
2024-03-11 02:10:48 +0000

    feat(type-utils): new package

---
+++ packages/test-utils/src/index.ts
@@ -0,0 +1,31 @@
+import { deferred } from "@disco/common";
+import http from "http";
+
+export async function getClientAndServer() {
+  const server = http.createServer().listen(0);
+  const port = await getPort(server);
+  const baseUrl = `http://localhost:${port}`;
+
+  return [
+    {
+      get(path: string) {
+        return fetch(`${baseUrl}${path}`);
+      },
+    },
+    server,
+  ] as const;
+}
+
+function getPort(server: http.Server) {
+  const { promise, resolve, reject } = deferred<number>();
+
+  server.on("listening", () => {
+    const addr = server.address();
+    if (addr && typeof addr !== "string") {
+      resolve(addr.port);
+    }
+    reject();
+  });
+
+  return promise;
+}
