ef8dc05bc8e8f2bb596c297ac8d978ad3c2685cc
Andrew Burgess <andrew.burgess@classdojo.com>
2024-03-14 01:48:24 +0000

    feat(common): add pipe

--- packages/common/src/index.test.ts
+++ packages/common/src/index.test.ts
@@ -1,5 +1,5 @@
 import { it, expect, describe } from "vitest";
-import { deferred } from "./index";
+import { deferred, pipe } from "./index";
 
 describe("deferred", () => {
   it("resolves the promise", () => {
@@ -14,5 +14,30 @@ describe("deferred", () => {
     const { reject, promise } = deferred();
     promise.catch((value) => expect(value).toEqual(1));
     reject(1);
+  });
+});
+
+describe("pipe", () => {
+  it("works", () => {
+    const x = pipe((n: number) => {
+      if (n > 0) return "positive" as const;
+      if (n < 0) return "negative" as const;
+      return "zero" as const;
+    })
+      .pipe((s) => {
+        switch (s) {
+          case "positive":
+            return 91;
+          case "negative":
+            return 82;
+          default:
+            throw "boom";
+        }
+      })
+      .pipe((n) => n % 10);
+
+    expect(x(20)).toBe(1);
+    expect(() => x(0)).toThrowError("boom");
+    expect(x(-20)).toBe(2);
   });
 });
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -11,3 +11,13 @@ export function deferred<T, E = unknown>() {
 
   return { promise, resolve, reject };
 }
+
+export function pipe<A, B>(fn: (a: A) => B) {
+  function run(a: A) {
+    return fn(a);
+  }
+
+  run.pipe = <C>(fn2: (b: B) => C) => pipe((a: A) => fn2(fn(a)));
+
+  return run;
+}
