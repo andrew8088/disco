572f43d467c22cce005b8baf9afc2d516ef30b05
Andrew Burgess <andrew.burgess@classdojo.com>
2024-04-09 14:06:24 +0000

    feat(manucci): transaction handling

--- packages/manucci/src/ledger.test.ts
+++ packages/manucci/src/ledger.test.ts
@@ -2,10 +2,36 @@ import { describe, expect, it } from "vitest";
 import { Ledger } from "./ledger";
 
 describe("manucci", () => {
-  it("works", () => {
+  it("works for simple transactions", () => {
     const ledger = new Ledger();
     ledger.addTransaction({ from: "Alice", to: "Bob", amount: 100 });
     expect(ledger.getBalance("Alice")).toBe(-100);
     expect(ledger.getBalance("Bob")).toBe(100);
+  });
+
+  it("balances across multiple transactions", () => {
+    const ledger = new Ledger();
+    ledger.addTransaction({ from: "Alice", to: "Bob", amount: 100 });
+    ledger.addTransaction({ from: "Alice", to: "Charlie", amount: 50 });
+    expect(ledger.getBalance("Alice")).toBe(-150);
+    expect(ledger.getBalance("Bob")).toBe(100);
+    expect(ledger.getBalance("Charlie")).toBe(50);
+
+    ledger.addTransaction({ from: "Bob", to: "Charlie", amount: 25 });
+    expect(ledger.getBalance("Bob")).toBe(75);
+    expect(ledger.getBalance("Charlie")).toBe(75);
+  });
+
+  it("handles complex transactions", () => {
+    const ledger = new Ledger();
+    ledger.addTransaction([
+      { from: "Alice", amount: 100 },
+      { to: "Bob", amount: 50 },
+      { to: "Charlie", amount: 50 },
+    ]);
+
+    expect(ledger.getBalance("Alice")).toBe(-100);
+    expect(ledger.getBalance("Bob")).toBe(50);
+    expect(ledger.getBalance("Charlie")).toBe(50);
   });
 });
--- packages/manucci/src/ledger.ts
+++ packages/manucci/src/ledger.ts
@@ -7,6 +7,14 @@ type SimpleTransaction = {
   amount: number;
 };
 
+type ComplexTransaction = (
+  | { from: string; amount: number }
+  | {
+      to: string;
+      amount: number;
+    }
+)[];
+
 export class Ledger {
   ledgerId: LedgerId;
   transactions: Transaction[] = [];
@@ -16,26 +24,50 @@ export class Ledger {
     this.ledgerId = LedgerId();
   }
 
-  addTransaction(transaction: SimpleTransaction) {
-    const entry1 = {
-      value: 0 - transaction.amount,
-      accountId: this.#getAccountId(transaction.from),
-      note: "",
-    };
+  addTransaction(transaction: SimpleTransaction | ComplexTransaction) {
+    if (Array.isArray(transaction)) {
+      const entries = transaction.map((entry) => {
+        if ("from" in entry) {
+          return {
+            value: 0 - entry.amount,
+            accountId: this.#getAccountId(entry.from),
+            note: "",
+          };
+        } else {
+          return {
+            value: entry.amount,
+            accountId: this.#getAccountId(entry.to),
+            note: "",
+          };
+        }
+      });
 
-    const entry2 = {
-      value: transaction.amount,
-      accountId: this.#getAccountId(transaction.to),
-      note: "",
-    };
+      this.transactions.push({
+        ledgerId: this.ledgerId,
+        transactionId: TransactionId(),
+        entries,
+        createdAt: new Date(),
+      });
+    } else {
+      const entry1 = {
+        value: 0 - transaction.amount,
+        accountId: this.#getAccountId(transaction.from),
+        note: "",
+      };
 
-    this.transactions.push({
-      ledgerId: this.ledgerId,
-      transactionId: TransactionId(),
-      entries: [entry1, entry2],
-      createdAt: new Date(),
-    });
+      const entry2 = {
+        value: transaction.amount,
+        accountId: this.#getAccountId(transaction.to),
+        note: "",
+      };
 
+      this.transactions.push({
+        ledgerId: this.ledgerId,
+        transactionId: TransactionId(),
+        entries: [entry1, entry2],
+        createdAt: new Date(),
+      });
+    }
     return this;
   }
 
