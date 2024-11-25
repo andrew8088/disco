d783d483bd31c3257cd8bc1f73c2c06a05b2356b
Andrew Burgess <andrew@burgess.io>
2024-11-25 02:42:55 +0000

    feat(dollar): select from and to account

--- apps/dollar/src/database.ts
+++ apps/dollar/src/database.ts
@@ -7,10 +7,10 @@ if (!DOLLAR_PATH) {
   throw new Error("DOLLAR_PATH is not set");
 }
 
-const db = new Database(DOLLAR_PATH, { verbose: console.log });
+const db = new Database(DOLLAR_PATH);
 
 export function findAccounts() {
-  const raw = db.prepare("SELECT id, name FROM accounts").all();
+  const raw = db.prepare("SELECT id, name, type, description FROM accounts").all();
   return accountParser.parse(raw);
 }
 
@@ -18,5 +18,13 @@ const accountParser = z.array(
   z.object({
     id: z.number(),
     name: z.string(),
+    type: z.or([
+      z.literal("asset"),
+      z.literal("expense"),
+      z.literal("liability"),
+      z.literal("income"),
+      z.literal("placeholder"),
+    ]),
+    description: z.string(),
   }),
 );
--- apps/dollar/src/index.ts
+++ apps/dollar/src/index.ts
@@ -1,3 +1,32 @@
-import { input } from "@inquirer/prompts";
+import { select } from "@inquirer/prompts";
+import { findAccounts } from "./database";
 
-input({ message: "Enter your name" }).then(console.log);
+async function main() {
+  const accounts = findAccounts();
+  const assets = accounts.filter(
+    (account) => account.type === "asset" || account.type === "liability",
+  );
+  const expenses = accounts.filter((account) => account.type === "expense");
+
+  const fromAccount = await select({
+    message: "Select a from account",
+    choices: assets.map((account) => ({
+      name: account.name,
+      value: account,
+      description: account.description,
+    })),
+  });
+
+  const toAccount = await select({
+    message: "Select a to account",
+    choices: expenses.map((account) => ({
+      name: account.name,
+      value: account,
+      description: account.description,
+    })),
+  });
+
+  console.log({ fromAccount, toAccount });
+}
+
+main();
