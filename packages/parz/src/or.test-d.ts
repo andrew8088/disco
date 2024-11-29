268a9629aa9ab0a31c0647080a19a6fe1d5b8c28
Andrew Burgess <andrew@burgess.io>
2024-11-29 20:35:53 +0000

    chore(parz): add tests

--- packages/parz/src/or.test-d.ts
+++ packages/parz/src/or.test-d.ts
@@ -1,9 +1,20 @@
 import { it, expectTypeOf } from "vitest";
 
 import literal from "./literal";
+import string from "./string";
 import or from "./or";
 
 it("returns union types", () => {
   const result1 = or([literal("42"), literal(false)]).parse("42");
   expectTypeOf(result1).toEqualTypeOf<"42" | false>();
+});
+
+it("works with null", () => {
+  const result1 = or([string(), literal(null)]).parse("42");
+  expectTypeOf(result1).toEqualTypeOf<string | null>();
+});
+
+it("works with undefined", () => {
+  const result1 = or([string(), literal(undefined)]).parse("42");
+  expectTypeOf(result1).toEqualTypeOf<string | undefined>();
 });
