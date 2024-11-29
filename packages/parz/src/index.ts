f67a72fb30dfd6c7b90c9e22dc8891374221c931
Andrew Burgess <andrew@burgess.io>
2024-11-29 19:31:58 +0000

    feat(parz): export new types

--- packages/parz/src/index.ts
+++ packages/parz/src/index.ts
@@ -1,5 +1,7 @@
 export { default as string } from "./string";
 export { default as number } from "./number";
+export { default as bigint } from "./bigint";
+export { default as date } from "./date";
 export { default as boolean } from "./boolean";
 export { default as literal } from "./literal";
 export { default as record } from "./record";
