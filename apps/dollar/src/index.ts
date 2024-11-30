f56a726d8c38f35da3ebb7895811960690d54f1d
Andrew Burgess <andrew@burgess.io>
2024-11-30 20:11:55 +0000

    feat(dollar): use commander for top-level commands

--- apps/dollar/src/index.ts
+++ apps/dollar/src/index.ts
@@ -1,6 +1,9 @@
 import * as Template from "./template";
 import * as Entry from "./entry";
 import { select } from "@inquirer/prompts";
+import { Command } from "commander";
+
+const program = new Command();
 
 const STANDARD_TEMPLATES: Template.Template[] = [
   {
@@ -18,21 +21,30 @@ const STANDARD_TEMPLATES: Template.Template[] = [
   },
 ];
 
-async function main() {
-  const templates = Template.find();
-  const template = await select({
-    message: "Select a template",
-    choices: Template.toChoices([...STANDARD_TEMPLATES, ...templates]),
+program
+  .name("dollar")
+  .description("A simple CLI for managing personal finances, using double-entry bookkeeping.");
+
+program
+  .command("add")
+  .description("Add a new transaction")
+  .action(async () => {
+    try {
+      const templates = Template.find();
+      const template = await select({
+        message: "Select a template",
+        choices: Template.toChoices([...STANDARD_TEMPLATES, ...templates]),
+      });
+
+      const entries = await Promise.all(template.transactions.map((t) => Entry.complete(t)));
+      entries.forEach((e) => Entry.create(e));
+    } catch (error) {
+      if (error instanceof Error && error.name === "ExitPromptError") {
+        console.log("👋 until next time!");
+      } else {
+        throw error;
+      }
+    }
   });
 
-  const entries = await Promise.all(template.transactions.map((t) => Entry.complete(t)));
-  entries.forEach((e) => Entry.create(e));
-}
-
-main().catch((error) => {
-  if (error instanceof Error && error.name === "ExitPromptError") {
-    console.log("👋 until next time!");
-  } else {
-    throw error;
-  }
-});
+program.parse();
