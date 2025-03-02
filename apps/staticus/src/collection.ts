d99b8a7018e5dd97003cbb1ca7301fef16e7ae92
Andrew Burgess <andrew@burgess.io>
2025-03-02 15:10:42 +0000

    feat(staticus): make buildable

--- apps/staticus/src/collection.ts
+++ apps/staticus/src/collection.ts
@@ -1,14 +1,20 @@
 import { Options } from "./staticus";
 
+type CollectionArg<R, T> = {
+  reader: (opts: Options) => AsyncIterable<R>;
+  transformer: (items: AsyncIterable<R>, opts: Options) => AsyncGenerator<T>;
+  writer: (items: AsyncIterable<T>, opts: Options) => Promise<void>;
+};
+
+export type Collection = {
+  build(opts: Options): Promise<void>;
+};
+
 export default function collection<R, T>({
   reader,
   transformer,
   writer,
-}: {
-  reader: (opts: Options) => AsyncIterable<R>;
-  transformer: (items: AsyncIterable<R>, opts: Options) => AsyncGenerator<T>;
-  writer: (items: AsyncIterable<T>, opts: Options) => Promise<void>;
-}) {
+}: CollectionArg<R, T>): Collection {
   return {
     async build(opts: Options) {
       await writer(transformer(reader(opts), opts), opts);
--- apps/staticus/src/staticus.ts
+++ apps/staticus/src/staticus.ts
@@ -1,12 +1,15 @@
+import { Collection } from "./collection";
+export * as collection from "./collection";
+export * as reader from "./reader";
+export * as transformers from "./transformers";
+export * as writer from "./writer";
+
 export type Options = {
   srcDir: string;
   destDir: string;
 };
 
-export default function staticus(
-  options: Options,
-  collections: Record<string, { build: (o: Options) => Promise<void> }>,
-) {
+export default function staticus(options: Options, collections: Record<string, Collection>) {
   return {
     async build() {
       for (const collection of Object.values(collections)) {
